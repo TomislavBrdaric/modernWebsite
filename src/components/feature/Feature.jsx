0c2760c02efc7b88ecf6400e0d7e11a4ac484a5e
Tomislav Brdarić <tbrdaric90@gmail.com>
2023-07-04 14:13:26 +0200

    added and finished the brand section and started with the feature section

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import './feature.css'
+import React from "react";
+import "./feature.css";
 
 const Feature = () => {
-  return (
-    <div>feature</div>
-  )
-}
+  return <div>feature</div>;
+};
 
-export default Feature
+export default Feature;
